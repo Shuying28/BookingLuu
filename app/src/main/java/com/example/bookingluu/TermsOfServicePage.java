78d93a6495c28ee7d8eeea2a3c8ad7b544f0a7fa
regina036 <reginatang306@gmail.com>
2022-12-08 20:02:35 +0800

    -card view for menu list -terms of service -card view for review (rating bar bug)

---
+++ app/src/main/java/com/example/bookingluu/TermsOfServicePage.java
@@ -0,0 +1,14 @@
+package com.example.bookingluu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermsOfServicePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_of_service_page);
+    }
+}
