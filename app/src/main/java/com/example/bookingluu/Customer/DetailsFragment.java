88f9d7c7f5e2e72947f559385d06520226ace9cd
ShuYing <ogngshuyiing@gmail.com>
2022-12-30 00:38:13 +0800

    Google Map API

--- app/src/main/java/com/example/bookingluu/Customer/DetailsFragment.java
+++ app/src/main/java/com/example/bookingluu/Customer/DetailsFragment.java
@@ -55,9 +55,8 @@ public class DetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false);
+         return inflater.inflate(R.layout.fragment_details, container, false);
     }
-
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -69,6 +68,5 @@ public class DetailsFragment extends Fragment {
         }
 
     }
-
 
 }
