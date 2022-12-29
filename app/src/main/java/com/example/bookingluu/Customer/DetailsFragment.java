77eec6e5ff3c36ae50379dd2db18f993c358b531
ShuYing <ogngshuyiing@gmail.com>
2022-12-29 23:22:08 +0800

    Google Map API

--- app/src/main/java/com/example/bookingluu/Customer/DetailsFragment.java
+++ app/src/main/java/com/example/bookingluu/Customer/DetailsFragment.java
@@ -2,21 +2,53 @@ package com.example.bookingluu.Customer;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 import com.example.bookingluu.R;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class DetailsFragment extends Fragment {
+    private GoogleMap gMap;
 
 
     public DetailsFragment() {
         // Required empty public constructor
     }
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * We just add a marker near Kolej Kediaman Kinabalu.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            gMap = googleMap;
+
+            //Add a marker in HollandFond Restaurant and move the camera
+            LatLng hollandLoc = new LatLng(3.1302616854935614, 101.6492878222151 );
+            gMap.addMarker(new MarkerOptions().position(hollandLoc).title("Holland Food"));
+            gMap.moveCamera(CameraUpdateFactory.newLatLng(hollandLoc));
+        }
+    };
 
 
     @Override
@@ -25,4 +57,18 @@ public class DetailsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.maps);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+
+    }
+
+
 }
